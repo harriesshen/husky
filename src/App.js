12952ea245d7780b5acad22235ee95b140b2f639
harries <has@ogloba.com>
2024-12-09 11:17:54 +0800

    [hotfix-123] hotfix123

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-    return <div className="App">husky test123</div>;
+    return (
+        <div className="App">
+            husky test123
+            <h1>Hot Fix 123</h1>
+        </div>
+    );
 }
 
 export default App;
